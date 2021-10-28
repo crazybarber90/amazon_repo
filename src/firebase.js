8fc88c285cc130f98ce790c39c597d9387a04405
crazybarber90 <pepy90aa@gmail.com>
2021-10-28 15:23:36 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC0qYR4do1FdF24fAKMCiCq0pypqj57jzM",
+    authDomain: "challenge-8beee.firebaseapp.com",
+    projectId: "challenge-8beee",
+    storageBucket: "challenge-8beee.appspot.com",
+    messagingSenderId: "458016747453",
+    appId: "1:458016747453:web:0e5051de4cac97ac70ecd4",
+    measurementId: "G-MVJ98DVLBZ"
+  };
